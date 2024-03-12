327146cb8d195c923cb1fece3f5545a1a29b7b52
Luthfi Fadle Rynaldy <mac@vdsl-130-13-10-105.phnx.qwest.net>
2024-03-13 00:54:38 +0700

    gambar pengantin

--- js/custom.js
+++ js/custom.js
@@ -58,6 +58,19 @@ $(document).ready(function() {
           itemsMobile : [479,1],
       });
     });
+
+  $(document).ready(function() {
+      $("#team-carousel2").owlCarousel({
+          items : 3,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3],
+          slideSpeed: 300,
+          itemsDesktop : [1199,2],
+          itemsTablet: [768,1],
+          itemsTabletSmall: [985,2],
+          itemsMobile : [479,1],
+      });
+    });
     
 
     /* Back to Top
